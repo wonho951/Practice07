314faf2ab02082e13fa3426079b2cf84e5113fbc
wonho <wonho951@gmail.com>
2021-06-10 15:29:19 +0900

    2021.06.10 추상 인터페이스 과제

--- src/com/javaex/ex02/Bird.java
+++ src/com/javaex/ex02/Bird.java
@@ -1,8 +1,36 @@
 package com.javaex.ex02;
 
 public abstract class Bird {
+	
+	//필드
     private String name;
 
+    //생성자
+	public Bird() {
+		super();
+	}
 
+	public Bird(String name) {
+		super();
+		this.name = name;
+	}
+	//메소드 g/s
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	
+	//메소드-일반
+	public abstract void showName();
+	
+	public abstract void sing();
+	
+	public abstract void fly();
+	
 
 }
