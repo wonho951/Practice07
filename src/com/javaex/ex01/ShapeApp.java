100c8f76f3d9e2ca340b1274bbb70b7f8b97d492
wonho <wonho951@gmail.com>
2021-06-10 15:17:44 +0900

    2021.06.10 추상 인터페이스 과제

--- src/com/javaex/ex01/ShapeApp.java
+++ src/com/javaex/ex01/ShapeApp.java
@@ -4,16 +4,16 @@ public class ShapeApp {
 
 	public static void main(String[] args) {
 
-		Shape s = new Shape("빨강");
+		//Shape s = new Shape("빨강"); abstract 붙으면 자기자신 출력 불가
 		
 		Circle c1 = new Circle("녹색", 10);
 		Ractangle r1 = new Ractangle("빨강", 4, 4);
-		Shape sr1 = new Ractangle("빨강", 4, 4);
+		Shape sr1 = new Ractangle("빨강", 12, 10);
 		
 		//sr1을 그리는 메소드 호출
 		sr1.draw();
 		//sr1의 가로크기 출력
-		System.out.println("가로크기: " + ((Ractangle)sr1));
+		System.out.println("sr1의 가로는 " + ((Ractangle)sr1).getWidth() + " 입니다.");
 	}
 }
 
