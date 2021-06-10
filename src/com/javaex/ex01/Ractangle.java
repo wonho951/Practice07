e9147159e2da73aab62d39e9e318525a83258b66
wonho <wonho951@gmail.com>
2021-06-10 15:08:49 +0900

    2021.06.10 추상 인터페이스 과제

---
+++ src/com/javaex/ex01/Ractangle.java
@@ -0,0 +1,28 @@
+package com.javaex.ex01;
+
+public class Ractangle extends Shape{
+	
+	private int width;
+	private int height;
+	
+	public Ractangle(String fillColor, int width, int height){
+		super(fillColor);
+		this.width=width;
+		this.height=height;
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	
+	public void draw(){
+		System.out.println("[사각형]#면색:" +super.fillColor+ " "
+				+ " #가로:" + width 
+				+" #세로:" + height + "그렸습니다.");                                                                        
+	}
+		
+
+}
+
+
